1d4351691210d349f39af13854ecf84797d83791
khw11044 <khw11044@naver.com>
2025-09-28 01:53:10 +0900

    [add] 양필로봇 : 텔레옵

--- src/lerobot/robots/__init__.py
+++ src/lerobot/robots/__init__.py
@@ -1,3 +1,14 @@
 from .config import RobotConfig
 from .robot import Robot
 from .utils import make_robot_from_config
+
+# Bimanual SO-101 follower
+from .bi_so101_follower import BiSO101Follower, BiSO101FollowerConfig
+
+__all__ = [
+    "RobotConfig",
+    "Robot",
+    "make_robot_from_config",
+    "BiSO101Follower",
+    "BiSO101FollowerConfig",
+]
